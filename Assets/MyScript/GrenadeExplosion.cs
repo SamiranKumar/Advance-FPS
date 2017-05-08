796f2b8f5d125f64f99298adcde2a560a9f33969
Samiran <samirankumar11@gmail.com>
2017-05-08 13:21:03 +0600

    Create Prefabe,Throw Grenate,collision detaction...

---
+++ Assets/MyScript/GrenadeExplosion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Chapter1{
+	public class GrenadeExplosion : MonoBehaviour {
+
+		private Collider[] hitColliders;
+		public float blastRadius;
+
+
+		void OnCollisionEnter(Collision col){
+			//mDebug (col.contacts [0].point.ToString ());
+
+			explosionWork (col.contacts [0].point); 
+			Destroy (gameObject);
+		}
+
+
+		void explosionWork(Vector3 explositionPoint){
+			hitColliders = Physics.OverlapSphere (explositionPoint, blastRadius);
+			foreach (Collider hitCol in hitColliders) {
+				mDebug (hitCol.gameObject.name);
+			}
+		
+		}
+		void mDebug(string message){
+			Debug.Log (message);
+		}
+	}
+
+}
---
+++ Assets/MyScript/ThrowGrenade.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Chapter1{
+	public class ThrowGrenade : MonoBehaviour {
+		public GameObject mGrenadePrefab;
+		private Transform mTransfron;
+
+		public float propulsionForce = 30;
+
+		// Use this for initialization
+		void Start () {
+			SetInitialReferences ();
+		}
+
+		// Update is called once per frame
+		void Update () {
+			if (Input.GetButtonDown ("Fire1")) {
+				spawnGrenade ();
+			}
+		}
+
+
+		void SetInitialReferences(){
+
+			mTransfron = transform;
+
+		}
+
+
+		void spawnGrenade(){
+			GameObject grenade = (GameObject) Instantiate(mGrenadePrefab, mTransfron.TransformPoint (0, 0, .5f), mTransfron.rotation);
+
+			grenade.GetComponent<Rigidbody> ().AddForce (mTransfron.forward * propulsionForce, ForceMode.Impulse);
+			Destroy(grenade ,10); // destory grenade after 10 sec
+
+		}
+		void mDebug(string message){
+			Debug.Log (message);
+		}
+	}
+
+}
+
+
